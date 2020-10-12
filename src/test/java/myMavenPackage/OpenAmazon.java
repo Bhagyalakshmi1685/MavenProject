70d1dcd24dfcf19639353295b45a791b35c65ebd
Bhagyalakshmi BD <lakshmi.lakshmi408@gmail.com>
2020-10-13 00:28:20 +0530

    first maven project commit

---
+++ src/test/java/myMavenPackage/OpenAmazon.java
@@ -0,0 +1,16 @@
+package myMavenPackage;
+
+
+public class OpenAmazon {
+
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 20;
+		
+		int sum = a+b;
+		System.out.println("sum of two nums is " +sum);
+
+	}
+
+}
